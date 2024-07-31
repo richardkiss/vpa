a0880249c53f185e82b345b142442ccfec365aa6
Richard Kiss <him@richardkiss.com>
2024-07-31 13:59:47 -0700

    annotation.py

--- vpa/virtual_project_analysis.py
+++ vpa/virtual_project_analysis.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import ast
 import json
 import os
-import re
 import sys
 from dataclasses import dataclass, field
 from pathlib import Path
@@ -12,27 +11,14 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 import click
 import yaml
 
+from .annotation import Annotation
 from .package_annotation_map import PATH_TO_PACKAGE
 
-@dataclass(frozen=True)
-class Annotation:
-    package: str
 
-    @classmethod
-    def is_annotated(cls, file_string: str) -> bool:
-        return file_string.startswith("# Package: ")
+ANNOTATION_MAP: dict[Path, Annotation] = {
+    Path(k): Annotation(v) for k, v in PATH_TO_PACKAGE.items()
+}
 
-    @classmethod
-    def parse(cls, file_string: str) -> Annotation:
-        result = re.search(r"^# Package: (.+)$", file_string, re.MULTILINE)
-        if result is None:
-            raise ValueError("Annotation not found")
-
-        return cls(result.group(1).strip())
-
-
-
-ANNOTATION_MAP : dict[Path, Annotation] = {Path(k): Annotation(v) for k, v in PATH_TO_PACKAGE.items()}
 
 @dataclass(frozen=True)
 class ChiaFile:
@@ -42,7 +28,11 @@ class ChiaFile:
     def parse(cls, file_path: Path) -> ChiaFile:
         with open(file_path, encoding="utf-8", errors="ignore") as f:
             file_string = f.read()
-            ANNOTATION_MAP[file_path] = Annotation.parse(file_string) if Annotation.is_annotated(file_string) else None
+            ANNOTATION_MAP[file_path] = (
+                Annotation.parse(file_string)
+                if Annotation.is_annotated(file_string)
+                else None
+            )
             return cls(file_path)
 
     @property
@@ -59,27 +49,42 @@ def build_dependency_graph(dir_params: DirectoryParameters) -> Dict[Path, List[P
             tree = ast.parse(filestring, filename=chia_file.path)
             for node in ast.iter_child_nodes(tree):
                 if isinstance(node, ast.ImportFrom):
-                    if node.module is not None and node.module.startswith(dir_params.dir_path.stem):
-                        imported_path = os.path.join(dir_params.dir_path.parent, node.module.replace(".", "/") + ".py")
+                    if node.module is not None and node.module.startswith(
+                        dir_params.dir_path.stem
+                    ):
+                        imported_path = os.path.join(
+                            dir_params.dir_path.parent,
+                            node.module.replace(".", "/") + ".py",
+                        )
                         paths_to_search = [
                             imported_path,
-                            *(os.path.join(imported_path[:-3], alias.name + ".py") for alias in node.names),
+                            *(
+                                os.path.join(imported_path[:-3], alias.name + ".py")
+                                for alias in node.names
+                            ),
                         ]
                         for path_to_search in paths_to_search:
                             if os.path.exists(path_to_search):
-                                dependency_graph[chia_file.path].append(Path(path_to_search))
+                                dependency_graph[chia_file.path].append(
+                                    Path(path_to_search)
+                                )
                 elif isinstance(node, ast.Import):
                     for alias in node.names:
                         if alias.name.startswith(dir_params.dir_path.stem):
                             imported_path = os.path.join(
-                                dir_params.dir_path.parent, alias.name.replace(".", "/") + ".py"
+                                dir_params.dir_path.parent,
+                                alias.name.replace(".", "/") + ".py",
                             )
                             if os.path.exists(imported_path):
-                                dependency_graph[chia_file.path].append(Path(imported_path))
+                                dependency_graph[chia_file.path].append(
+                                    Path(imported_path)
+                                )
     return dependency_graph
 
 
-def build_virtual_dependency_graph(dir_params: DirectoryParameters) -> Dict[str, List[str]]:
+def build_virtual_dependency_graph(
+    dir_params: DirectoryParameters,
+) -> Dict[str, List[str]]:
     graph = build_dependency_graph(dir_params)
     virtual_graph: Dict[str, List[str]] = {}
     for file, imports in graph.items():
@@ -90,7 +95,9 @@ def build_virtual_dependency_graph(dir_params: DirectoryParameters) -> Dict[str,
         virtual_graph.setdefault(root, [])
 
         dependency_files = [ChiaFile.parse(Path(imp)) for imp in imports]
-        dependencies = [f.annotations.package for f in dependency_files if f.annotations is not None]
+        dependencies = [
+            f.annotations.package for f in dependency_files if f.annotations is not None
+        ]
 
         virtual_graph[root].extend(dependencies)
 
@@ -107,7 +114,9 @@ def is_excluded(file_path: Path, excluded_paths: List[Path]) -> bool:
         if file_path == excluded_path:
             return True
         # Check if the excluded path is a directory and if the file is under this directory
-        if excluded_path.is_dir() and any(parent == excluded_path for parent in file_path.parents):
+        if excluded_path.is_dir() and any(
+            parent == excluded_path for parent in file_path.parents
+        ):
             return True
 
     return False
@@ -117,15 +126,21 @@ def is_excluded(file_path: Path, excluded_paths: List[Path]) -> bool:
 # graph: A dictionary mapping each file to its list of dependencies.
 # excluded_paths: A list of paths that should be excluded from the cycle detection.
 # ignore_cycles_in: A list of package names where cycles, if found, should be ignored.
-def find_cycles(graph: Dict[Path, List[Path]], excluded_paths: List[Path], ignore_cycles_in: List[str]) -> List[str]:
-
+def find_cycles(
+    graph: Dict[Path, List[Path]],
+    excluded_paths: List[Path],
+    ignore_cycles_in: List[str],
+) -> List[str]:
     # Define a nested function for recursive dependency searching.
     # top_level_package: The name of the package at the top of the dependency tree.
     # left_top_level: A boolean flag indicating whether we have moved beyond the top-level package in our traversal.
     # dependency: The current dependency path being examined.
     # already_seen: A list of dependency paths that have already been visited to avoid infinite loops.
     def recursive_dependency_search(
-        top_level_package: str, left_top_level: bool, dependency: Path, already_seen: List[Path]
+        top_level_package: str,
+        left_top_level: bool,
+        dependency: Path,
+        already_seen: List[Path],
     ) -> List[List[Tuple[str, Path]]]:
         # Check if the dependency is in the list of already seen dependencies or is excluded.
         if dependency in already_seen or is_excluded(dependency, excluded_paths):
@@ -145,14 +160,18 @@ def find_cycles(graph: Dict[Path, List[Path]], excluded_paths: List[Path], ignor
             return [[(chia_file.annotations.package, dependency)]]
         else:
             # Update the left_top_level flag if we have moved to a different package.
-            left_top_level = left_top_level or chia_file.annotations.package != top_level_package
+            left_top_level = (
+                left_top_level or chia_file.annotations.package != top_level_package
+            )
             # Recursively search through all dependencies of the current dependency and accumulate the results.
             return [
                 [(chia_file.annotations.package, dependency), *stack]
                 for stack in [
                     _stack
                     for dep in graph[dependency]
-                    for _stack in recursive_dependency_search(top_level_package, left_top_level, dep, already_seen)
+                    for _stack in recursive_dependency_search(
+                        top_level_package, left_top_level, dep, already_seen
+                    )
                 ]
             ]
 
@@ -163,20 +182,36 @@ def find_cycles(graph: Dict[Path, List[Path]], excluded_paths: List[Path], ignor
         # Parse the parent package file.
         chia_file = ChiaFile.parse(parent)
         # Skip this package if it has no annotations or should be ignored in cycle detection.
-        if chia_file.annotations is None or chia_file.annotations.package in ignore_cycles_in:
+        if (
+            chia_file.annotations is None
+            or chia_file.annotations.package in ignore_cycles_in
+        ):
             continue
         # Extend the path_accumulator with results from the recursive search starting from this parent.
-        path_accumulator.extend(recursive_dependency_search(chia_file.annotations.package, False, parent, []))
+        path_accumulator.extend(
+            recursive_dependency_search(
+                chia_file.annotations.package, False, parent, []
+            )
+        )
 
     # Format and return the accumulated paths as strings showing the cycles.
-    return [" -> ".join([str(d) + f" ({p})" for p, d in stack]) for stack in path_accumulator]
+    return [
+        " -> ".join([str(d) + f" ({p})" for p, d in stack])
+        for stack in path_accumulator
+    ]
 
 
 def print_graph(graph: Union[Dict[str, List[str]], Dict[Path, List[Path]]]) -> None:
-    print(json.dumps({str(k): list(str(v) for v in vs) for k, vs in graph.items()}, indent=4))
+    print(
+        json.dumps(
+            {str(k): list(str(v) for v in vs) for k, vs in graph.items()}, indent=4
+        )
+    )
 
 
-@click.group(help="A utility for grouping different parts of the repo into separate projects")
+@click.group(
+    help="A utility for grouping different parts of the repo into separate projects"
+)
 def cli() -> None:
     pass
 
@@ -197,7 +232,11 @@ class DirectoryParameters:
         python_files = []
         for root, dirs, files in os.walk(self.dir_path, topdown=True):
             # Modify dirs in-place to remove excluded directories from search
-            dirs[:] = [d for d in dirs if Path(os.path.join(root, d)) not in self.excluded_paths]
+            dirs[:] = [
+                d
+                for d in dirs
+                if Path(os.path.join(root, d)) not in self.excluded_paths
+            ]
 
             for file in files:
                 file_path = Path(os.path.join(root, file))
@@ -254,12 +293,16 @@ def config(func: Callable[..., None]) -> Callable[..., None]:
         # Instantiate DirectoryParameters with the provided options
         dir_params = DirectoryParameters(
             dir_path=Path(kwargs.pop("include_dir")),
-            excluded_paths=[*(Path(p) for p in kwargs.pop("excluded_paths")), *exclude_paths],
+            excluded_paths=[
+                *(Path(p) for p in kwargs.pop("excluded_paths")),
+                *exclude_paths,
+            ],
         )
 
         # Instantiating the Config object
         config = Config(
-            directory_parameters=dir_params, ignore_cycles_in=[*kwargs.pop("ignore_cycles_in", []), *ignore_cycles_in]
+            directory_parameters=dir_params,
+            ignore_cycles_in=[*kwargs.pop("ignore_cycles_in", []), *ignore_cycles_in],
         )
 
         # Calling the wrapped function with the Config object and other arguments
@@ -268,7 +311,10 @@ def config(func: Callable[..., None]) -> Callable[..., None]:
     return inner
 
 
-@click.command("find_missing_annotations", short_help="Search a directory for chia files without annotations")
+@click.command(
+    "find_missing_annotations",
+    short_help="Search a directory for chia files without annotations",
+)
 @config
 def find_missing_annotations(config: Config) -> None:
     flag = False
@@ -281,21 +327,27 @@ def find_missing_annotations(config: Config) -> None:
         sys.exit(1)
 
 
-@click.command("print_dependency_graph", short_help="Output a dependency graph of all the files in a directory")
+@click.command(
+    "print_dependency_graph",
+    short_help="Output a dependency graph of all the files in a directory",
+)
 @config
 def print_dependency_graph(config: Config) -> None:
     print_graph(build_dependency_graph(config.directory_parameters))
 
 
 @click.command(
-    "print_virtual_dependency_graph", short_help="Output a dependency graph of all the packages in a directory"
+    "print_virtual_dependency_graph",
+    short_help="Output a dependency graph of all the packages in a directory",
 )
 @config
 def print_virtual_dependency_graph(config: Config) -> None:
     print_graph(build_virtual_dependency_graph(config.directory_parameters))
 
 
-@click.command("print_cycles", short_help="Output cycles found in the virtual dependency graph")
+@click.command(
+    "print_cycles", short_help="Output cycles found in the virtual dependency graph"
+)
 @click.option(
     "--ignore-cycles-in",
     "ignore_cycles_in",
