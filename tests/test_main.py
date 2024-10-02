d173f75b6c96a710955eab2075f8caa2602a0ab0
Richard Kiss <him@richardkiss.com>
2024-10-01 20:21:54 -0700

    Additional basic tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,30 +10,39 @@ TEST_DIR = Path(__file__).parent
 print(TEST_DIR)
 
 
-def test_print_cycles():
-    test_proj_dir = TEST_DIR / "test_proj"
+def do_test(command: str, expected_output: str):
     runner = CliRunner()
     with runner.isolated_filesystem() as base:
-        # need to cp -r test_proj_dir base
         test_dir = Path(base) / "test_proj"
-        shutil.copytree(test_proj_dir, test_dir)
-        r = runner.invoke(cli, ["--directory", test_dir, "print_cycles"])
+        shutil.copytree(TEST_DIR / "test_proj", test_dir)
+        r = runner.invoke(cli, ["--directory", test_dir, command])
         print(r.output)
         assert r.exit_code == 0
-        assert r.output == "\n"
+        assert r.output == expected_output
+
+
+def test_print_cycles():
+    do_test("print_cycles", "\n")
 
 
 def test_find_cycles():
-    test_proj_dir = TEST_DIR / "test_proj"
-    runner = CliRunner()
-    with runner.isolated_filesystem() as base:
-        # need to cp -r test_proj_dir base
-        test_dir = Path(base) / "test_proj"
-        shutil.copytree(test_proj_dir, test_dir)
-        r = runner.invoke(cli, ["--directory", test_dir, "find_cycles"])
-        print(r.output)
-        assert r.exit_code == 0
-        assert (
-            r.output
-            == "cycle of length 2 found from a.py (['b.py', 'a.py'])\n  b.py -> a.py (b -> a)\n  a.py -> b.py (a -> b)\ncycle of length 2 found from b.py (['a.py', 'b.py'])\n  a.py -> b.py (a -> b)\n  b.py -> a.py (b -> a)\n"
-        )
+    do_test(
+        "find_cycles",
+        "cycle of length 2 found from a.py (['b.py', 'a.py'])\n  b.py -> a.py (b -> a)\n  a.py -> b.py (a -> b)\ncycle of length 2 found from b.py (['a.py', 'b.py'])\n  a.py -> b.py (a -> b)\n  b.py -> a.py (b -> a)\n",
+    )
+
+
+def test_print_edges():
+    do_test("print_edges", "('a.py', 'b.py')\n('b.py', 'a.py')\n")
+
+
+def test_print_leafs():
+    do_test("print_leafs", "[]\n")
+
+
+def test_print_missing_annotations():
+    do_test("print_missing_annotations", "a.py\nb.py\n")
+
+
+def test_print_virtual_dependency_graph():
+    do_test("print_virtual_dependency_graph", "{}\n")
